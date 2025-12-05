71594ba2684af7781e9ff5700181cd8e8e603d8a
HeeHo Lim <121926482+hiho1010@users.noreply.github.com>
2025-12-05 11:32:11 +0900

    first commit

---
+++ src/main/java/org/todaybook/bookpreprocessingworker/BookpreprocessingworkerApplication.java
@@ -0,0 +1,13 @@
+package org.todaybook.bookpreprocessingworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookpreprocessingworkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookpreprocessingworkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/todaybook/bookpreprocessingworker/BookpreprocessingworkerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.todaybook.bookpreprocessingworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookpreprocessingworkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
